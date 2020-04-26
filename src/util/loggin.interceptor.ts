5acf8fa0cded196f6626ffc0235b8a70f4f00fce
Pavan Mantha <Pavan.Mantha@Honeywell.com>
2020-04-26 10:52:29 +0530

    added logger capability for graphql

--- src/util/loggin.interceptor.ts
+++ src/util/loggin.interceptor.ts
@@ -1,6 +1,7 @@
 import {CallHandler, ExecutionContext, Logger, NestInterceptor} from '@nestjs/common';
 import {Observable} from "rxjs";
 import {tap} from "rxjs/operators";
+import {GqlExecutionContext} from "@nestjs/graphql";
 
 export class LogginInterceptor implements NestInterceptor {
 
@@ -12,7 +13,11 @@ export class LogginInterceptor implements NestInterceptor {
             const past: number = Date.now();
             return next.handle().pipe(tap(() => Logger.log(`${method} ${url} ${Date.now() - past} ms`, context.getClass().name)));
         }else{
-            return next.handle();
+            const ctx = GqlExecutionContext.create(context);
+            const resolverName = ctx.getContext().constructorRef;
+            const info = ctx.getInfo()
+            const past: number = Date.now();
+            return next.handle().pipe(tap(() => Logger.log(`[${info.parentType}] ${info.fieldName} ${Date.now() - past} ms`, resolverName)));
         }
     }
 }
